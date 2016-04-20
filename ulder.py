232312c295ce4360be726e92519dcfb2376f4bc0
Henry <the.seedh@gmail.com>
2016-04-20 19:14:13 +1200

    Added input prompt

--- ulder.py
+++ ulder.py
@@ -1,10 +1,13 @@
 
 class Grid():
     
-    def __init__(self,x,y):
+    def __init__(self,x,y, start):
         self.x = x
         self.y = y
         self.layout = [[' '] * x] * y
+        
+        current_pos = start
+        
         
     def __str__(self):
         
@@ -31,8 +34,44 @@ class Grid():
         grid += row + '\n'
                     
         return grid
+    
+    
+    def run(self, inpt):
+        print('running')
         
+    
+def control(grid, inpt):
+    
+    error = False
+    
+    options = ['u','l','d','r']
+    
+    for i in inpt:
+        if i not in options:
+            error = True
+    
+    if error == False:
+        return grid.run(inpt)
         
+    else:
+        print('error in input u = up, l = left, d = down, r = right')
+    
+    
         
-uldr = Grid(3, 1)
-print(uldr)
+            
+def uldr():
+    
+    quit = False
+    
+    print()
+    grid = Grid(3,1,(0,0))
+    print(grid)
+    
+    while quit == False:
+        prompt = input('> ')
+        
+        control(grid, prompt)
+
+
+uldr()
+        
